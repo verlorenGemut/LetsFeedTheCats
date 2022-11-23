9876e15b96bd8d2d70cd5730433f251428036de5
Julia Zasemkova <zasemkovajulia@gmail.com>
2022-11-23 19:59:36 +0300

    Add welcome form

---
+++ LetsFeedTheCatsProject/LetsFeedTheCatsProject/WelcomeScreen.cs
@@ -0,0 +1,15 @@
+namespace LetsFeedTheCatsProject
+{
+    public partial class WelcomeScreen : Form
+    {
+        public WelcomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void WelcomeScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
